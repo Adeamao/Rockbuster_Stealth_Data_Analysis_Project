39163f0512ba842733b0e8080502d3eea52f27bc
Adeamao <adeamao@gmail.com>
2024-06-20 22:12:38 +0200

    Create Country, Number of Customers, And Revenue Contribution.sql

---
+++ Country, Number of Customers, And Revenue Contribution.sql
@@ -0,0 +1,18 @@
+SELECT 
+    F.country,
+    COUNT(DISTINCT C.customer_id) AS customer_count,
+    SUM(P.amount) AS total_revenue
+FROM 
+    customer C
+JOIN 
+    address D ON C.address_id = D.address_id
+JOIN 
+    city E ON D.city_id = E.city_id
+JOIN 
+    country F ON E.country_id = F.country_id
+JOIN
+    payment P ON C.customer_id = P.customer_id
+GROUP BY 
+    F.country
+ORDER BY 
+    customer_count DESC;
