21dbeb34a345db66f2dc00fc63e5d5e859c2ca23
Adeamao <adeamao@gmail.com>
2024-06-20 22:08:16 +0200

    Create Total Number of Films.sql

---
+++ Total Number of Films.sql
@@ -0,0 +1,15 @@
+SELECT DISTINCT 
+    title,
+    description,
+    film_id,
+    release_year,
+    language_id,
+    rental_duration,
+    rental_rate,
+    length,
+    replacement_cost,
+    rating,
+    last_update
+FROM 
+    film;
+
