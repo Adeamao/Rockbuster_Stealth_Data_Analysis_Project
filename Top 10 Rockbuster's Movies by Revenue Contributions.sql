d9c5e9018dbed2750279980103446c4ac358cc2f
Adeamao <adeamao@gmail.com>
2024-06-20 22:26:13 +0200

    Create Top 10 Rockbuster's Movies by Revenue Contributions.sql

---
+++ Top 10 Rockbuster's Movies by Revenue Contributions.sql
@@ -0,0 +1,17 @@
+SELECT 
+    f.title,
+    SUM(p.amount) AS total_revenue,
+    f.rental_duration
+FROM 
+    payment p
+JOIN 
+    rental r ON p.rental_id = r.rental_id
+JOIN 
+    inventory i ON r.inventory_id = i.inventory_id
+JOIN 
+    film f ON i.film_id = f.film_id
+GROUP BY 
+    f.title, f.rental_duration
+ORDER BY 
+    total_revenue DESC
+LIMIT 10;
